1b79a0ea75f7c38363be3e798a456a531275cb7f
Matt Gaunt <mattgaunt@google.com>
2018-06-29 08:32:59 -0700

    Improving install flow

--- ubuntu-setup.sh
+++ ubuntu-setup.sh
@@ -35,9 +35,11 @@ function setupGit() {
 function installNode() {
   # Install Node and NPM - https://nodejs.org/en/download/package-manager/#debian-and-ubuntu-based-linux-distributions
   echo -e "📦  Installing Node.js..."
-  NODE_VERSION=10
-  curl -sL "https://deb.nodesource.com/setup_${NODE_VERSION}.x" | sudo -E bash - &> ${ERROR_LOG}
-  sudo apt-get install -y nodejs &> ${ERROR_LOG}
+  if ! [ -x "$(command -v node)" ]; then
+    NODE_VERSION=10
+    curl -sL "https://deb.nodesource.com/setup_${NODE_VERSION}.x" | sudo -E bash - &> ${ERROR_LOG}
+    sudo apt-get install -y nodejs &> ${ERROR_LOG}
+  fi
   echo -e "\n\t✅  Done\n"
 }
 
@@ -58,7 +60,7 @@ function setupZSHRC() {
   ZSH_FILE="${HOME}/.zshrc"
   
   if [ -L "${ZSH_FILE}" ] || [ -f "${ZSH_FILE}" ] ; then
-    sudo rm "${ZSH_FILE}"
+    rm "${ZSH_FILE}" &> ${ERROR_LOG}
   fi
 
   echo -e "source ${DOTFILES_DIR}/assets/zshrc" > "${ZSH_FILE}"
