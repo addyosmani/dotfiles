c26c172e114fdf31515320e2958fa3317b4045c4
Matt Gaunt <mattgaunt@google.com>
2018-10-21 10:34:20 -0700

    Cleanup after vscode install

--- setup.sh
+++ setup.sh
@@ -162,6 +162,7 @@ function installChrome() {
       Linux*)
           curl --silent https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &> ${ERROR_LOG}
           sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/  &> ${ERROR_LOG}
+          rm microsoft.gpg &> ${ERROR_LOG}
           if [ ! -f /etc/apt/sources.list.d/vscode.list ]; then
 	    sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' &> ${ERROR_LOG}
 	  fi
