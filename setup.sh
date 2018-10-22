7ae8621e8c93d7919cfd1584cb4530d96e257316
Matt Gaunt <mattgaunt@google.com>
2018-10-21 17:19:00 -0700

    Hopefully fixing the VSCode server key issue

--- setup.sh
+++ setup.sh
@@ -161,9 +161,9 @@ function installVSCode() {
   unameOut="$(uname -s)"
   case "${unameOut}" in
       Linux*)
-          curl --silent https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg &> ${ERROR_LOG}
-          sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/  &> ${ERROR_LOG}
-          sudo mv microsoft.gpg /etc/apt/trusted.gpg.d/microsoft.gpg  &> ${ERROR_LOG}
+          curl https://packages.microsoft.com/keys/microsoft.asc | gpg --dearmor > microsoft.gpg
+          sudo install -o root -g root -m 644 microsoft.gpg /etc/apt/trusted.gpg.d/
+          rm microsoft.gpg
           if [ ! -f /etc/apt/sources.list.d/vscode.list ]; then
 	          sudo sh -c 'echo "deb [arch=amd64] https://packages.microsoft.com/repos/vscode stable main" > /etc/apt/sources.list.d/vscode.list' &> ${ERROR_LOG}
 	        fi
