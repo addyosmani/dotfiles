2dc397c42eb2f6e9156090c15fa2c5579242b46f
Matt Gaunt <mattgaunt@google.com>
2019-01-06 20:52:44 -0800

    Removing old files

--- old/quick-keys/6.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/bash
-
-notify-send "Quick Key 6";
--- old/quick-keys/8.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-notify-send "Quick Key 8: Open Spotify";
-
-google-chrome \
-  --profile-directory="Default" \
-  --app="https://player.spotify.com/"
