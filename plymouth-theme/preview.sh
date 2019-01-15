7e8c71af0ab01580d8b25adf54ebb87553f478d9
Matt Gaunt <mattgaunt@google.com>
2019-01-15 09:15:47 -0800

    Refactoring plymouth theme to animate correctly

--- plymouth-theme/preview.sh
+++ plymouth-theme/preview.sh
@@ -5,11 +5,11 @@ if [ $EUID -ne 0 ]; then
 	exit
 fi
 
-plymouthd --debug 
+plymouthd --debug --debug-file=/home/matt/Projects/Tools/dotfiles/plymouth.debug
 plymouth --show-splash
 for ((I=0;I<10;I++))
 do
   	sleep 1s
-	plymouth --update=event$I
+	# plymouth --update=event$I
 done
 plymouth quit
